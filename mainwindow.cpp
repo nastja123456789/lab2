b5ebc7a359909e5dbd7244c2995508d28dedc6d8
nastja123456789 <60920907+nastja123456789@users.noreply.github.com>
2020-12-06 22:32:15 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -28,7 +28,7 @@ HashSet<Restaurant> currentHash;
 
 void MainWindow::on_pushButton_delete_all_clicked()
 {
-    currentHash.clear();
+    current.clear();
     ui->listWidget->clear();
 }
 
@@ -57,18 +57,13 @@ void MainWindow::on_pushButton_add_ok_clicked()
 
 void MainWindow::on_pushButton_delete_clicked()
 {
+
     ui->listWidget->clear();
 
-    for (auto it = current.begin(); it != current.end(); ++it) {
-           if (it->getName() == ui->resDel->text().toStdString()) {
-               it = current.erase(it);
-           }
-    }
-    //current.pop(Restaurant(ui->resDel->text().toStdString()));
+    remove(current.begin(), current.end(), Restaurant(ui->resDel->text().toStdString()));
 
-    /*for (Restaurant i: current) {
-        current.erase(i); //remove(Restaurant(ui->resDel->text().toStdString()));
-    }*/
+    current.pop_back();
+
 
     for (Restaurant i: current) {
         ui->listWidget->addItem("Restaurant: " +
@@ -85,97 +80,7 @@ void MainWindow::on_pushButton_delete_clicked()
 void MainWindow::on_pushButton_file_input_ok_clicked()
 {
 
-        QFile fileIn("filein.txt");
-        QFile fileOut("fileout.txt");
-        if(fileIn.open(QIODevice::ReadOnly) && fileOut.open(QIODevice::WriteOnly))
-        { //Если первый файл открыт для чтения, а второй для записи успешн
-            QByteArray block = fileIn.read(10); // Считываем 10 байт в массив block из filein.txt
-            fileOut.write(block); // Записываем 10 байт в файл fileout.txt
-            fileIn.close(); // Закрываем filein.txt
-            fileOut.close(); // Закрываем fileout.txt
-        }
-   /* currentHash.saveToFile("C:\\Qt\\a.txt");
-    QFile f( ui->lineEdit_file_input_name->text() );
-    f.open( QIODevice::WriteOnly | QIODevice::Text );
 
-    QTextStream strm( &f );
-
-    //Node<element*> *n = stackData.top_ptr;
-    for (Restaurant i: current) {
-
-
-
-    while (n != NULL)
-    {
-        strm << n->el->getType() << "\n";
-        n->el->write( strm );
-
-        n = n->next;
-    }
-
-    strm.flush();
-    f.close();
-    //currentHash.loadFromFile("f.txt");
-
-    //QTextStream out(stdout);
-    //QString filename = "f.txt";
-
-    //QFile file(filename);
-
-    /*ifstream file1("f.txt");
-
-    if (file1.is_open())
-    {
-
-        for (Restaurant i: current) {
-
-            out << "aaaa" << endl;
-
-         }
-
-        file1.close();
-    }*/
-
-
-    /*std::ofstream fs(filename);
-    fs.write((char *) &m_size, sizeof(m_size) );
-
-    for (Restaurant i: current) {
-            int s;
-            std::string ks = i.toString();
-            s = ks.size();
-            fs.write((char *) &(s), sizeof(int));
-            fs.write((char *) ks.c_str(), ks.size());
-
-        }
-            fs.close();*/
-
-    /*if(file.open(QIODevice::WriteOnly)) {
-        QTextStream out(&file);
-
-
-
-        for (Restaurant i: current) {
-
-            out << i.getName() << endl;*/
-
-           // и передаем ему адрес объекта fileOut
-                   //writeStream << "Text, text, text."; // Посылаем строку в поток для записи
-                  // fileOut.close(); // Закрываем файл
-
-            //file << i.toString() << endl;
-
-            //file << i;
-            //file.write(i.toString());
-           //file.write(i.toString());
-            /*ui->listWidget->addItem("Restaurant: " +
-                                QString::fromStdString( i.toString()));
-        }
-
-   }
-    //file.close();
-    QMessageBox::information(this, "Input data", "Successful input data from file");
-    ui->lineEdit_file_input_name->clear();*/
 }
 
 
@@ -190,10 +95,60 @@ void MainWindow::on_pushButton_file_output_ok_clicked()
 
 void MainWindow::on_pushButton_add_ok_2_clicked()
 {
+    current = currentHash.round();
 
+     ui->listWidget->clear();
+
+     for (Restaurant i: current) {
+         ui->listWidget->addItem("Restaurant: " +
+                             QString::fromStdString( i.toString()));
+     }
 }
 
 void MainWindow::on_addToSet_clicked()
 {
     currentHash.addCurrent(current);
+
+    current.clear();
+
+     ui->listWidget->clear();
+}
+
+void MainWindow::on_pushButton_file_input_ok_2_clicked()
+{
+
+   QFile fileOut("C:\\Qt\\l\\HashSet\\fileout.txt"); // Связываем объект с файлом fileout.txt
+       if(fileOut.open(QIODevice::Append | QIODevice::Text))
+       { // Если файл успешно открыт для записи в текстовом режиме
+          // QMessageBox::information(this, "Thi1s","restaurant have been");
+           QTextStream writeStream(&fileOut);
+
+           for (Restaurant i: current) {
+
+               std::string str = i.getName();
+               const char* p = str.c_str();
+
+               writeStream << p << "\n";
+           }
+
+           writeStream << "\n";
+
+           fileOut.close();
+       }
+       /*
+        * string name = ui->lineEdit_file_input_name->text().toStdString();
+    if(name.size())
+    {
+        this->collection.clear();
+        this->collection.input_from_file(name);
+        for(auto &i: this->collection.get())
+            for(auto &j: i)
+                ui->listWidget->addItem("Word: " +
+                                    QString::fromStdString(j.first) +
+                                    "\nCount: " +
+                                    QString::number(j.second));
+    }
+    QMessageBox::information(this, "Input data", "Successful input data from file");
+    ui->lineEdit_file_input_name->clear();
+        */
 }
